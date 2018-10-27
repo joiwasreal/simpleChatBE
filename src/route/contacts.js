0f65c26012f4e2101858226d8b6e56e625e71614
Romuald Bulyshko <romuald@bulyshko.com>
2018-10-27 13:38:03 +0300

    Add contact management

--- src/route/contacts.js
+++ src/route/contacts.js
@@ -1,8 +1,38 @@
 const Router = require('koa-router')
 const router = new Router({ prefix: '/contacts' })
+const body = require('koa-body')
+const validate = require('../middleware/validate')
+const knex = require('../db')
 
 router.get('/', async ctx => {
-  ctx.body = []
+  const data = await knex('contacts')
+    .where({ user_id: ctx.params.userId })
+    .join('users', 'users.id', '=', 'contacts.contact_id')
+    .select('users.id', 'users.username', 'users.name')
+
+  ctx.body = {
+    status: 'success',
+    data
+  }
+})
+
+router.post('/', body(), validate(require('../model/id')), async ctx => {
+  await knex('contacts').insert({
+    user_id: ctx.params.userId,
+    contact_id: ctx.request.body.id
+  })
+  ctx.status = 201
+  ctx.body = {
+    status: 'success'
+  }
+})
+
+router.delete('/:contactId', async ctx => {
+  const { userId, contactId } = ctx.params
+
+  await knex('contacts').where({ user_id: userId, contact_id: contactId }).delete()
+
+  ctx.status = 204
 })
 
 router.use('/:contactId', require('./messages').routes())
