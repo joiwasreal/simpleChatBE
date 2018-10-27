7d8a29c66226057d5a2c216ee732d4ea7b19f172
Romuald Bulyshko <romuald@bulyshko.com>
2018-10-27 14:58:34 +0300

    IDs are unknown

--- seeds/02_contacts.js
+++ seeds/02_contacts.js
@@ -1,29 +1,33 @@
 exports.seed = async knex => {
+  const users = {};
+  for (let user of await knex('users').select('id', 'username')) {
+    users[user.username] = user.id;
+  }
   await knex('contacts').truncate()
   await knex('contacts').insert([
     {
-      user_id: 1,
-      contact_id: 2
+      user_id: users.bulyshko,
+      contact_id: users.uldis
     },
     {
-      user_id: 2,
-      contact_id: 1
+      user_id: users.uldis,
+      contact_id: users.bulyshko
     },
     {
-      user_id: 6,
-      contact_id: 7
+      user_id: users.larry,
+      contact_id: users.sergey
     },
     {
-      user_id: 7,
-      contact_id: 6
+      user_id: users.sergey,
+      contact_id: users.larry
     },
     {
-      user_id: 1,
-      contact_id: 4
+      user_id: users.bulyshko,
+      contact_id: users.steve
     },
     {
-      user_id: 1,
-      contact_id: 3
+      user_id: users.bulyshko,
+      contact_id: users.jeff
     }
   ])
 }
