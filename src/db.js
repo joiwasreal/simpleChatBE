ea81a29f6885f99c50a66a9db4ee2343c82009f1
Romuald Bulyshko <romuald@bulyshko.com>
2018-11-02 18:41:38 +0200

    ORM is an overkill

--- src/db.js
+++ src/db.js
@@ -1,6 +1,4 @@
-const { Model } = require('objection')
 const Knex = require('knex')
 const { NODE_ENV = 'development' } = process.env
 const knex = Knex(require('../knexfile')[NODE_ENV])
-Model.knex(knex)
 module.exports = knex
