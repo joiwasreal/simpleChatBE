73b8c50cb7f07f7726f937011a01b5d3aa462ad6
Romuald Bulyshko <romuald@bulyshko.com>
2018-11-17 16:01:14 +0200

    Add CORS middleware

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 const Koa = require('koa')
 const favicon = require('koa-favicon')
+const cors = require('koa2-cors')
 
 const { PORT = 3000 } = process.env
 
 const app = new Koa()
 
 app.use(favicon(`${__dirname}/favicon.ico`))
+app.use(cors())
 app.use(require('./middleware/ssl'))
 app.use(require('./middleware/error'))
 
