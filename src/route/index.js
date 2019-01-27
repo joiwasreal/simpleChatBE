e4d32eeae65ee25f8e56ae011716564f7958aa3d
Romuald Bulyshko <romuald@bulyshko.com>
2019-01-27 18:21:31 +0200

    Redirect to repository instead of README

--- src/route/index.js
+++ src/route/index.js
@@ -1,6 +1,6 @@
 const Router = require('koa-router')
 const router = new Router()
 
-router.get('/', ctx => ctx.redirect('https://github.com/bulyshko/up/blob/master/README.md'))
+router.get('/', ctx => ctx.redirect('https://github.com/bulyshko/up'))
 
 module.exports = router
