1058cefea337faf06a59422ec75e3a64fee7f10e
joi <uldis.plotins@gmail.com>
2019-01-27 21:26:52 +0200

    Update chats.js

--- src/route/chats.js
+++ src/route/chats.js
@@ -5,7 +5,8 @@ const knex = require('../db')
 router.get('/', async ctx => {
   const data = await knex('messages').where({
     recipient_id: ctx.params.userId
-  }).select('sender_id AS user_id', 'created_at')    
+  }).select('sender_id AS user_id')    
+    .max('created_at AS created_at')
     .groupBy('sender_id')
     .orderBy('sender_id', 'desc')
 
