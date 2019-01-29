772cc8ae706de5e273b11043267923a507ff52ad
joi <uldis.plotins@gmail.com>
2019-01-29 11:50:46 +0200

    Update chats.js

--- src/route/chats.js
+++ src/route/chats.js
@@ -1,19 +1,1 @@
-const Router = require('koa-router')
-const router = new Router({ prefix: '/chats' })
-const knex = require('../db')
 
-router.get('/', async ctx => {
-  const data = await knex('messages').where({
-    recipient_id: ctx.params.userId
-  }).select('sender_id AS user_id')    
-    .max('created_at AS created_at')
-    .groupBy('sender_id')
-    .orderBy('sender_id', 'asc')
-
-  ctx.body = {
-    status: 'success',
-    data
-  }
-})
-
-module.exports = router
